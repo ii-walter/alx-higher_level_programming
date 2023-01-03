63b4e885c4f5cdd0fa0bc6bc60e5026b35acdacb
Walter Ibekwe <walterifeanyichukwy@gmail.com>
2023-01-03 12:21:09 -0800

    Objects

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
-This is a module that containts a clas that avoids
+This is a module that containts a class that avoids
 dynmaically created attributes
 """
 
 
 class LockedClass:
-    __slots__ = ['first_name']
+    __slots__ = ['last_name']
 
     def __init__(self):
         """ Init method """
